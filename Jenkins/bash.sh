24e29d9c020077b695652ec1f43da11e48fce7a9
Dishone Prabu J <dishoneprabu959@gmail.com>
2023-01-29 16:32:40 +0530

    jenkins

---
+++ Jenkins/bash.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+apt update && apt upgrade -y >> /etc/bash-logs/log.txt
+
+apt install docker.io  >> /etc/bash-logs/log.txt
+
+docker --version >> /etc/bash-logs/log.txt
+
+docker run -d -h Jenkins --name Jenkins -p 8080:8080 -p 50000:50000 --restart=on-failure -v jenkins_home:/var/jenkins_home jenkins/jenkins
